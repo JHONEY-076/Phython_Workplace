f7023214062d995be55d35cdd121f16c1c935746
JHONEY-076 <ccjh2593gmail.com>
2024-09-07 18:22:43 +0900

    commit 09-07

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ print("\\\\")
 print("'")
 print("\"\"\"")
 print("C:\nin\the dowonward spiral")
-print("/ \\ // \\\\ /// \\\\\\)"
+print("/ \\ // \\\\ /// \\\\\\")
+
+print("This quote is from\n Trish poet Oscar Wilde:\n\n\"Music makes one feel so romantic\n- at least it always gets on one's nervers -\nwhich is the same thing nowadays.\"")
+
+
+print("A \"quoted\" String is\n \'much\' better if you learn\nthe rules of \"esacpe sequences.")
+print()
+print("Also, \"\" represents an empty String.\n Dont forget: use \ \" instead of \" !\n\'\' is not the same as \" ")
