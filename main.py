6b9b51c83e3b1ef76336c8cc67c3fc3143faa9e1
JHONEY-076 <ccjh2593@gmail.com>
2024-09-09 12:06:50 +0900

    09-09

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ print("This quote is from\n Trish poet Oscar Wilde:\n\n\"Music makes one feel so
 print("A \"quoted\" String is\n \'much\' better if you learn\nthe rules of \"esacpe sequences.")
 print()
 print("Also, \"\" represents an empty String.\n Dont forget: use \ \" instead of \" !\n\'\' is not the same as \" ")
+print("Hello")
