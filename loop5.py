6082c62eba3f91bed4c08ec659f071c8aad21d49
JHONEY-076 <ccjh2593@gmail.com>
2024-10-04 16:05:34 +0900

    10-04

--- loop5.py
+++ loop5.py
@@ -1,62 +1,158 @@
+# size=4
+# 
+# def main():
+#    
+#    draw_line()
+#    draw_top()
+#    draw_bottom()
+#    draw_line()
+#    
+#    
+# def draw_line():
+#    print("#================#")
+# 
+# 
+# def draw_top():
+#      for line in range(1,size+1):
+#       print("|", end="")
+#       
+#       for j in range(-2*line + (2*size)):
+#          print(" ",end="")
+#       
+#       print("<>",end="")
+#       
+#       for k in range(4*line - 4):
+#          print(".",end="")
+#          
+#       print("<>",end="")   
+#       for a in range(-2*line + (2*size)):
+#          print(" ",end="") 
+#          
+#       print("|")     
+#  
+#       
+#       
+# 
+# 
+# 
+# def draw_bottom():
+#    for line in range(1,size+1):
+#       print("|", end ="")
+#     
+#       for i in range(2*line-2):
+#          print(" ", end="")
+#  
+#       print("<>",end="")
+#       
+#       for j in range(-4*line+size*4):
+#          print(".",end="")
+#       
+#       print("<>",end="")
+#       
+#       for k in range(2*line-2):
+#          print(" ",end="")
+#       
+#       
+#       print("|", end="")
+#       
+#       print()
+# 
+# 
+# main()
+#       
+
+
 size=3
 
 def main():
-   
    draw_line()
    draw_top()
    draw_bottom()
-   draw_line()
    
    
+
 def draw_line():
    print("#================#")
-
-
+   
 def draw_top():
-     for line in range(1,size+1):
-      print("|", end="")
-      
-      for j in range(-2*line + (2*size)):
+   for line in range(1,size+1):
+      print("|",end="")
+   
+      for space in range(-2*line+(2*size)):
          print(" ",end="")
       
       print("<>",end="")
-      
-      for k in range(4*line - 4):
+   
+      for dot in range(4*line-4):
          print(".",end="")
-         
-      print("<>",end="")   
-      for a in range(-2*line + (2*size)):
-         print(" ",end="") 
-         
-      print("|")     
- 
       
+      print("<>",end="")
       
-
-
+      for space in range(-2*line+(2*size)):
+         print(" ",end="")
+         
+      print("|",end="")   
+         
+         
+      print()   
+   
 
 def draw_bottom():
    for line in range(1,size+1):
-      print("|", end ="")
-    
-      for i in range(2*line-2):
-         print(" ", end="")
- 
-      print("<>",end="")
-      
-      for j in range(-4*line+size*4):
-         print(".",end="")
-      
-      print("<>",end="")
-      
-      for k in range(2*line-2):
-         print(" ",end="")
-      
-      
       print("|", end="")
       
-      print()
-
-
-main()
+      for space in range(2*line-2):
+         print(" ",end="")
+         
+      print("<>",end="")
       
+      for dot in range(-4*line+(size*4)):
+         print(".",end="")
+         
+      print("<>",end="")
+      
+      for space in range(2*line-2):
+         print(" ",end="")
+      
+      print("|",end="")   
+         
+      print()   
+         
+
+
+
+
+
+   
+main()
+
+
+
+# jusuck solved => ctrl+shift+/
+
+# 
+# def main():
+#    draw_line()
+#    draw_star_space()
+#    draw_line()
+#    
+#    
+#    
+#    
+# def draw_line():
+#    print("************")
+#    
+#    
+# def draw_star_space():
+#    for i in range(5):
+#       print("*",end="")
+#       print(" "*10,end="")
+#       print("*",end="")
+#       print()
+#         
+# 
+# 
+# main()
+
+
+
