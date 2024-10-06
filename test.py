9875674ca667de9521ad3f0a9e09f206d33e8891
JHONEY-076 <ccjh2593gmail.com>
2024-10-06 14:44:22 +0900

    10-11

--- test.py
+++ test.py
@@ -1,26 +1,105 @@
-<<<<<<< HEAD
-print("\"-\"-\"-\"-\"")
-print("\\       \\")
-print("/       /")
-print("/       /")
-print("\\       \\")
-print("\"-\"-\"-\"-\"")
+SIZE=5
 
-print(12 // 7 * 4.4 * 2 // 4)
-=======
 def main():
+  
+   draw_top()
+   draw_line()
+   draw_body()
+   draw_line()
+   draw_body2()
+   draw_line()
    draw_top()
    
-   
 def draw_top():
-   print("     \'\'\'*\\ ")
    
-   for i in range(1,5):
-      for space in range(-1*i+5):
-         print("1 ",end="")
-      print()
-   
+   for i in range(1,SIZE*2):
+      for j in range(-1*i+(SIZE*2)):
+         print(" ",end="")
+         
+      for k in range(1*i):
+         print("/",end="")
+         
+      
+      print("**",end="")
             
-   
-main()      
->>>>>>> d67eb3aafc74ba08d18e1150bd557af60ac93bb0
+      for a in range(1*i):
+         print("\\",end="")
+         
+      print()
+
+
+def draw_line():
+      print("+",end="")
+      for i in range(SIZE*2):  
+         print("=*",end="")
+              
+      print("+")   
+
+def draw_body():
+   for i in range(1,SIZE+1):
+      print("|",end="")
+      for dot in range(-1*i+(SIZE)):
+         print(".",end="")
+      for slash in range(i):
+         print("/\\",end="")
+      for dots in range(-2*i+(SIZE*2)):
+         print(".",end="")
+      for slash2 in range(i):
+         print("/\\",end="")
+      for dots in range(-1*i+(SIZE)):
+         print(".",end="")
+      print("|", end="")   
+      print()   
+         
+   for i in range(1,SIZE+1):
+       print("|",end="")
+       for dot in range(1*i-1):
+          print(".",end="")
+       for slash in range(-1*i+(SIZE+1)):
+          print("\\/",end="")
+       for dots in range(2*i-2):
+          print(".",end="")
+       for slash2 in range(-1*i+(SIZE+1)):
+          print("\\/",end="")
+       for dots in range(1*i-1):
+          print(".",end="")
+       print("|", end="")   
+       print()   
+            
+                  
+            
+def draw_body2():
+   for i in range(1,SIZE+1):
+       print("|",end="")
+       for dot in range(1*i-1):
+          print(".",end="")
+       for slash in range(-1*i+(SIZE+1)):
+          print("\\/",end="")
+       for dots in range(2*i-2):
+          print(".",end="")
+       for slash2 in range(-1*i+(SIZE+1)):
+          print("\\/",end="")
+       for dots in range(1*i-1):
+          print(".",end="")
+       print("|", end="")   
+       print()   
+
+   for i in range(1,SIZE+1):
+      print("|",end="")
+      for dot in range(-1*i+(SIZE)):
+         print(".",end="")
+      for slash in range(i):
+         print("/\\",end="")
+      for dots in range(-2*i+(SIZE*2)):
+         print(".",end="")
+      for slash2 in range(i):
+         print("/\\",end="")
+      for dots in range(-1*i+(SIZE)):
+         print(".",end="")
+      print("|", end="")   
+      print()
+
+                     
+      
+
+main()
