d67eb3aafc74ba08d18e1150bd557af60ac93bb0
JHONEY-076 <ccjh2593@gmail.com>
2024-10-11 20:12:08 +0900

    10-11

---
+++ test.py
@@ -0,0 +1,15 @@
+def main():
+   draw_top()
+   
+   
+def draw_top():
+   print("     \'\'\'*\\ ")
+   
+   for i in range(1,5):
+      for space in range(-1*i+5):
+         print("1 ",end="")
+      print()
+   
+            
+   
+main()      
