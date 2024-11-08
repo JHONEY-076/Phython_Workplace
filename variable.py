91435cc978a083338722867d6edb79b600abd5e7
JHONEY-076 <ccjh2593@gmail.com>
2024-11-08 10:09:14 +0900

    11-08

--- variable.py
+++ variable.py
@@ -1,15 +1,13 @@
-def print_design():
+HEIGHT=4
 
-   for i in range(1, 10, 2):
-    spaces = (11 - i) // 2 
-    print('-' * spaces + str(i) * i + '-' * spaces)
-
-
-
-<<<<<<< HEAD
-=======
-print_design()
->>>>>>> f7ea98dec7646309d572f5496a45aa876354e05b
-
-
-print(str(9 + 9) + "9 + 9" + str(9) + str(9))
+for i in range(1,HEIGHT+1):
+   for j in range(i-1):
+      print("          ",end="")
+   print("+=========+")
+   for j in range(i-1):
+      print("          ",end="")
+   print("+=========+")
+   for j in range(i-1):
+      print("          ",end="")
+   print("+=========+")
+      
