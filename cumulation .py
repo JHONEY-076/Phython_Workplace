7bfc84d8ef39180b3cce25e1394a64ab8af1a9d3
JHONEY-076 <ccjh2593@gmail.com>
2024-11-08 20:22:55 +0900

    11-08

---
+++ cumulation .py
@@ -0,0 +1,104 @@
+# 
+# # add num 2
+# sum=0
+# for i in range(1,11):
+#    print(sum)
+#    sum+=i
+#    
+#    
+# print("Cumulative sum:" ,sum)
+# 
+# 
+
+
+# #2 to the 6th power
+# base=2
+# exp=6
+# res=1
+# for i in range(exp):
+#    print(res)
+#    res=res*base
+# print(res)
+
+
+# 
+# sum=0
+# for i in range(5):
+#    sum+=int(input("Enter one number:"))
+# print(sum)
+
+
+
+
+
+
+#
+# num=int(input("How mney people ate?"))
+# result=0
+# for i in range(1,num+1):
+#    print("Person #",i,":",end="")
+#    result+=float(input("How much did your dinner cost?"))
+#    
+# print()   
+# print("Subtotal:",result)
+# print("Tax",result*0.08)
+# print("Tip",result*0.15)
+# print("Total",result+(result)*0.15+(result)*0.08)
+#    
+
+
+
+# 
+# 
+# def main():
+#    people=int(input("How mney people ate? "))
+#    subtotal=0
+#    for i in range(1,people+1):
+#       print("Person #"+str(i)+":",end="")
+#       print("How much did your dinner cost?",end="")
+#       subtotal +=float(input())
+#   print("subtotal:",subtotal)   
+#    
+#    
+#    
+#    
+# main() 
+
+
+
+
+
+# sum=""
+# for i in range(ord("a"),ord("d")):
+#    sum+=chr(i)
+# print(sum)
+
+
+
+
+# message= input("Your secret message:")
+# key= int(input("Your secret key:"))
+# 
+# message=message.upper()
+# 
+# for i in range(len(message)):
+#    if message[i]==" ":
+#       continue
+#    print(chr(ord(message[i])+key),end="")
+#    
+# 
+
+text="abc xyz"
+key=1
+text=text.upper()
+
+for letter in text:
+   if letter>= "a" and letter<="Z":
+      shifted_num=ord(letter)+key
+      if shifted_num>ord("z"):
+         shifted_num=shifted_num%ord('[')+ord('A')
+      print(chr(shifted_num),end="")
+   else:
+      print(letter, end="")
+
+   
